5ec06edd679b6c8a04dfe197ee9621c8ec0800dc
yelim <kyl656227@gmail.com>
2026-02-27 12:58:19 +0900

    세부수정

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -38,7 +38,7 @@ const init = (userId) => {
         followingCount: 0,
         followState: 0,
     };
-    state.isMyProfile = userId == authenticationStore.state.signedUser.userId;
+    state.isMyProfile = userId === authenticationStore.state.signedUser.userId;
 };
 
 init(route.params.userId);
@@ -114,23 +114,21 @@ const onClickFollow = async () => {
     switch (state.userProfile.followState) {
         case 0:
         case 2: //post
-        const postRes = await postUserFollow({
-            toUserId: state.userProfile.userId
-        });
-        if (postRes.status === 200) {
-            state.userProfile.followState += 1;
-            state.userProfile.followerCount += 1;
-        }
-        break;
+            const postRes = await postUserFollow({ toUserId: state.userProfile.userId });
+            if (postRes.status === 200) {
+                state.userProfile.followState += 1;
+                state.userProfile.followerCount += 1;
+            }
+            break;
         default: //delete
-        const deleteRes = await deleteUserFollow({
-            to_user_id: state.userProfile.userId,
-        });
-        if (deleteRes.status === 200) {
-            state.userProfile.followState -= 1;
-            state.userProfile.followerCount -= 1;
-        }
-        break;
+            const deleteRes = await deleteUserFollow({
+                to_user_id: state.userProfile.userId,
+            });
+            if (deleteRes.status === 200) {
+                state.userProfile.followState -= 1;
+                state.userProfile.followerCount -= 1;
+            }
+            break;
     }
 };
 
